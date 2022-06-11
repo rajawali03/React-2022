3c0b9f5d92b8d0e7a39033672cce80f36197cdf4
Himiko <fahriramadhan@student.untan.ac.id>
2022-06-11 11:12:45 +0700

    make it modular

--- src/Test.js
+++ src/Test.js
@@ -2,6 +2,7 @@ import resiskom from "./assets/image/info-resiskom.svg";
 import himpunan from "./assets/image/info-himpunan.svg";
 import kepanitian from "./assets/image/info-kepanitian.svg";
 import akademik from "./assets/image/info-akademik.svg";
+import { Card } from "./Components/Card";
 export default function Test() {
   return (
     <div style={{ height: "100vh", backgroundColor: "red" }}>
@@ -19,40 +20,6 @@ export default function Test() {
         <Card image={kepanitian} text="Info Kepanitian" />
         <Card image={akademik} text="Info Akademik" />
       </div>
-    </div>
-  );
-}
-
-function Card({ image = "", text = "Info Resiskom" }) {
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "fit-content",
-        borderRadius: "1rem",
-        border: "10px solid white",
-        textAlign: "center",
-        backgroundColor: "#DCDCDC",
-        overflow: "hidden",
-      }}
-    >
-      <div
-        style={{
-          backgroundColor: "#EBEBEB",
-        }}
-      >
-        {image && (
-          <img
-            src={image}
-            alt="Cover"
-            style={{ width: "auto", borderRadius: "1rem" }}
-          />
-        )}
-      </div>
-      <h3 style={{ padding: "1rem 0" }}>{text && text}</h3>
     </div>
   );
 }
